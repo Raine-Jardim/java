1a3cb04c889109f41e9882b3bfd47f99f90c6ea3
Raine-Jardim <raine.jardim@sptech.school>
2024-08-08 22:52:10 -0300

    Projeto da primeira aula de java

---
+++ projeto01/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        // Tipagem estática
+
+        String nome = "Raíne";
+        Integer idade = 19;
+        Long numero = 1000000000000L; // números grandes (tem que botar um L se não ele vai achar que é int)
+        Double altura = 1.58; // Tem mais bits, ocupa mais espaço, consegue representar números maiores, normalmente usa o double
+        Float alturaF = 1.58f; // tem que botar o F pq se não ele vai achar que é double
+        Boolean bloqueado = false;
+
+    }
+}
